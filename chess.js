c652de892e53778b5a84f7fbfb33890ea3e32f2f
Saheed Oladosu <oladosu777@gmail.com>
2019-01-15 12:02:23 +0100

    little addition

---
+++ chess.js
@@ -0,0 +1,11 @@
+let size = 8;
+let chess = " ";
+for(let i = 0; i < size; i++){
+	for(let j = 0; j < size; j++){
+		if((i+j)%2 == 0){
+			chess += " ";
+		} else{
+			chess += "#";
+		}
+	} chess += "\n"
+}
